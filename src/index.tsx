41ca77cc0f4d4198051bab87d21a08b2c95d61e7
Giuseppe Setem <giuseppe7d@gmail.com>
2022-04-18 19:19:30 -0300

    feat: add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
@@ -12,10 +13,12 @@ const entry = document.getElementById('root')
 const root = createRoot(entry!)
 
 root.render(
-  <ThemeProvider theme={Theme}>
-    <ReduxProvider store={store}>
-      <GlobalStyle />
-      <Router />
-    </ReduxProvider>
-  </ThemeProvider>
+  <StrictMode>
+    <ThemeProvider theme={Theme}>
+      <ReduxProvider store={store}>
+        <GlobalStyle />
+        <Router />
+      </ReduxProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
